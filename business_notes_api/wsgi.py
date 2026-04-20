4de0918c14e31b2a8cc79749fb2ad5eeb92165b5
unknown <ippililalith@gmail.com>
2026-04-20 11:03:19 +0530

    update latest code 4/20/2026 1

--- business_notes_api/wsgi.py
+++ business_notes_api/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'business_notes_api.settings')
 
 application = get_wsgi_application()
+application = app
